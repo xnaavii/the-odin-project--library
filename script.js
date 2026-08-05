a0fe3f5128cfc5cbccd0d47885fbea446d8f8db7
Ivan Petrović <137888945+xnaavii@users.noreply.github.com>
2026-08-05 13:06:59 +0100

    Write functions that create an add book object

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const myLibrary = [];
 
-function Book() {}
+function Book(id, title, author, pages) {
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
 
-function addBookToLibrary() {}
+  this.id = id;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
+
+function addBookToLibrary(title, author, pages) {
+  const newBook = new Book(crypto.randomUUID(), title, author, pages);
+  myLibrary.push(newBook);
+}
