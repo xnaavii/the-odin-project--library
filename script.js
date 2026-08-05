e83d61442c5dbf3385ce5c951bac3dbfb5417b51
Ivan Petrović <137888945+xnaavii@users.noreply.github.com>
2026-08-05 20:00:09 +0100

    Create a function to create new book card and add it to display

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+const library = document.querySelector('.library');
 const myLibrary = [];
+
+const dummyBooks = [
+  { title: 'To Kill a Mockingbird', author: 'Harper Lee', pages: 281 },
+  { title: '1984', author: 'George Orwell', pages: 328 },
+  { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', pages: 180 },
+  { title: 'Pride and Prejudice', author: 'Jane Austen', pages: 279 },
+  { title: 'The Hobbit', author: 'J.R.R. Tolkien', pages: 310 },
+  { title: 'Fahrenheit 451', author: 'Ray Bradbury', pages: 256 },
+];
 
 function Book(id, title, author, pages) {
   if (!new.target) {
@@ -11,22 +21,42 @@ function Book(id, title, author, pages) {
   this.pages = pages;
 }
 
-function addBookToLibrary(title, author, pages) {
-  const newBook = new Book(crypto.randomUUID(), title, author, pages);
+function addBookToLibrary(book) {
+  const newBook = new Book(
+    crypto.randomUUID(),
+    book.title,
+    book.author,
+    book.pages,
+  );
   myLibrary.push(newBook);
 }
 
-const dummyBooks = [
-  { title: 'To Kill a Mockingbird', author: 'Harper Lee', pages: 281 },
-  { title: '1984', author: 'George Orwell', pages: 328 },
-  { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', pages: 180 },
-  { title: 'Pride and Prejudice', author: 'Jane Austen', pages: 279 },
-  { title: 'The Hobbit', author: 'J.R.R. Tolkien', pages: 310 },
-  { title: 'Fahrenheit 451', author: 'Ray Bradbury', pages: 256 },
-];
+function createBookCard(book) {
+  const article = document.createElement('article');
+  const title = document.createElement('h4');
+  const author = document.createElement('p');
+  const pages = document.createElement('p');
 
-dummyBooks.forEach((book) => {
-  addBookToLibrary(book.title, book.author, book.pages);
-});
+  title.textContent = book.title;
+  author.textContent = book.author;
+  pages.textContent = book.pages;
 
-console.log(myLibrary)
+  article.append(title);
+  article.append(author);
+  article.append(pages);
+
+  return article;
+}
+
+if (dummyBooks.length > 0) {
+  dummyBooks.forEach((book) => {
+    addBookToLibrary(book);
+  });
+}
+
+if (myLibrary.length > 0) {
+  myLibrary.forEach((book) => {
+    const newBook = createBookCard(book);
+    library.append(newBook);
+  });
+}
